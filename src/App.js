17c810d9df2c3e5f5026ab024b3b10f2fa2ebfc5
mdcrys <78097538+mdcrys@users.noreply.github.com>
2021-04-07 17:51:11 -0500

    Crud en React

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { useState } from 'react'
 import {  isEmpty  } from 'lodash'
-function App() {  
-  
-  const [task, setTask] = useState(null)
+import shortid from 'shortid'
 
+function App() {
+  
+  const [task, setTask] = useState("")
+
+  const [tasks, setTasks] = useState([])
+
+  
   const addTask = (e) =>{
     e.preventDefault()
     if(isEmpty(task))
@@ -11,8 +16,16 @@ function App() {
       console.log("Task empty")
       return      
     }
-    console.log("ok")
-    setTask(null)
+   
+    const newTask = {
+      id: shortid.generate(),
+      name: task
+    }
+
+    setTasks([...tasks, newTask])
+
+
+    setTask("")
       
   }
   return (
@@ -23,17 +36,22 @@ function App() {
       <div className="row">
           <div className="col-8">
             <h4 className="text-center"> Lista de tareas</h4>
-        <ul className="list-group">
-                <li className="list-group-item">
-                  <span className="lead" >Nombre de la tarea</span>
-                  <button className="btn btn-danger btn-sm float-right mx-2 ">  Eliminar</button>
-                  <button className="btn btn-warning btn-sm float-right ">  editar</button>
+          <ul className="list-group">
+            {
+              tasks.map((task) => (
+                <li className="list-group-item" key={task.id}>
+                  <span className="lead" >{task.name }</span>
+                <button className="btn btn-danger btn-sm float-right mx-2 ">  Eliminar</button>
+                <button className="btn btn-warning btn-sm float-right ">  editar</button>
               </li>
+              ))
+              
+            }
             </ul>
           </div>
           <div className="col-4">
                <h4 className="text-center"> Formulario</h4>
-              <from onSubmit={addTask}>
+              <form onSubmit={addTask}>
                     <input 
                       type="text"
                       className="form-control mb-2"
@@ -48,7 +66,7 @@ function App() {
 
                   Agregar
                     </button> 
-              </from>
+              </form>
           </div>  
       </div>
     </div>
