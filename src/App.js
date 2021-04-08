fb7d603d244a3114bd54b57f0a45bbf51d2d927f
mdcrys <78097538+mdcrys@users.noreply.github.com>
2021-04-07 19:38:25 -0500

    crud en memoria

--- src/App.js
+++ src/App.js
@@ -28,6 +28,11 @@ function App() {
     setTask("")
       
   }
+
+  const deleteTask = (id) => {
+    const filteredTasks = tasks.filter(task => task.id !== id)
+    setTasks(filteredTasks)
+   }
   return (
    
     <div className="container mt-5"> 
@@ -41,8 +46,15 @@ function App() {
               tasks.map((task) => (
                 <li className="list-group-item" key={task.id}>
                   <span className="lead" >{task.name }</span>
-                <button className="btn btn-danger btn-sm float-right mx-2 ">  Eliminar</button>
-                <button className="btn btn-warning btn-sm float-right ">  editar</button>
+                  <button
+                    className="btn btn-danger btn-sm float-right mx-2 "
+                    onClick={()=> deleteTask(task.id)}
+                  >
+                    Eliminar</button>
+                  <button
+                    className="btn btn-warning btn-sm float-right "
+                  >
+                    Editar</button>
               </li>
               ))
               
